849d69a82a03b4dc9a155898b12b5596a518f36d
Rushikesh Tarapure <61598260+t007rushi@users.noreply.github.com>
2021-09-27 23:35:08 +0530

    final build commit

--- app.js
+++ app.js
@@ -5,10 +5,26 @@ var outputtext = document.querySelector(".optext")
 // res.addEventListener("click", function ToDoevent() {
 //     console.log("clicked!!!")
 // })
+var url = "https://api.funtranslations.com/translate/minion.json"
+
+function getURL(text) {
+    // url for API +query selector + key + value passed
+    return url + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+}
 
 function newfun() {
     // console.log(inptext.value);
-    outputtext.innerHTML = "new lang " + inptext.value
+    var inputText = inptext.value;
+    fetch(getURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var val = json.contents.translated;
+            outputtext.innerText = val;
+        }).catch(errorHandler)
 }
 
 res.addEventListener("click", newfun)
