4594576c6d4662cb6b7cff125bdb2cf6d6f0e96e
Rushikesh Tarapure <61598260+t007rushi@users.noreply.github.com>
2021-09-27 16:18:49 +0530

    i/p-o/p

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var res = document.querySelector(".clicked")
+var inptext = document.querySelector("#text")
+var outputtext = document.querySelector(".optext")
 
-console.log(res)
+// res.addEventListener("click", function ToDoevent() {
+//     console.log("clicked!!!")
+// })
 
-res.addEventListener("click", function ToDoevent() {
-    console.log("clicked!!!")
-})
+function newfun() {
+    // console.log(inptext.value);
+    outputtext.innerHTML = "new lang " + inptext.value
+}
+
+res.addEventListener("click", newfun)
